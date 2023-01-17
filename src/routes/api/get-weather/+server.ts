d0d78904112727086172e1f9f3e3b6c49e2160bb
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2023-01-17 17:58:34 -0300

    Format Code

--- src/routes/api/get-weather/+server.ts
+++ src/routes/api/get-weather/+server.ts
@@ -1,8 +1,7 @@
-
-import type { IRequestOptions } from "src/types/requestOptions";
-import type { WeatherResponse } from "src/types/weather";
-import type { RequestEvent } from "../$types";
-import { API_KEY, API_HOST } from '$env/static/private'
+import type { IRequestOptions } from 'src/types/requestOptions';
+import type { WeatherResponse } from 'src/types/weather';
+import type { RequestEvent } from '../$types';
+import { API_KEY, API_HOST } from '$env/static/private';
 
 const FETCH_OPTIONS: IRequestOptions = {
 	method: 'GET',
@@ -13,25 +12,34 @@ const FETCH_OPTIONS: IRequestOptions = {
 };
 
 /** @type {import('./$types').RequestHandler} */
-export async function GET({url}: RequestEvent) {
-	const query = url.searchParams.get('q') ?? 'Tucuman'
+export async function GET({ url }: RequestEvent) {
+	const query = url.searchParams.get('q') ?? 'Tucuman';
 
-  const response = await fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`, FETCH_OPTIONS)
+	const response = await fetch(
+		`https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`,
+		FETCH_OPTIONS
+	);
 
-	const data: WeatherResponse = await response.json()
+	const data: WeatherResponse = await response.json();
 
-	return new Response(JSON.stringify({
-		conditionIcon: data.current.condition.icon,
-		conditionText: data.current.condition.text,
-		country: data.location.country,
-		localtime: data.location.localtime,
-		locationName: data.location.name,
-		humidity: data.current.humidity,
-		isDay: data.current.is_day,
-		feelsLike: data.current.feelslike_c,
-		temperature: data.current.temp_c,
-		windSpeed: data.current.wind_kph,
-		windDir: data.current.wind_dir
-  }, null, 2),{status: 200})
-
+	return new Response(
+		JSON.stringify(
+			{
+				conditionIcon: data.current.condition.icon,
+				conditionText: data.current.condition.text,
+				country: data.location.country,
+				localtime: data.location.localtime,
+				locationName: data.location.name,
+				humidity: data.current.humidity,
+				isDay: data.current.is_day,
+				feelsLike: data.current.feelslike_c,
+				temperature: data.current.temp_c,
+				windSpeed: data.current.wind_kph,
+				windDir: data.current.wind_dir
+			},
+			null,
+			2
+		),
+		{ status: 200 }
+	);
 }
