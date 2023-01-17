d50eeba0b4b0c6edfebc7d0d3c260f3fb2afc857
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2023-01-17 17:59:13 -0300

    Added layout with initial style config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,25 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
 
-<div class='bg-slate-800 min-h-screen flex justify-center items-center'>
-  <slot />
+<div class="flex w-full justify-center items-center">
+	<slot />
 </div>
+
+<style>
+	:global(body) {
+		margin: 0;
+		display: flex;
+		justify-content: center;
+		min-height: 100vh;
+		background-color: rgb(30 41 59);
+		color: white;
+	}
+
+	:global(*, *::after, *::before) {
+		box-sizing: border-box;
+	}
+	:global(h1, h2, h3, h4, h5, h6) {
+		margin: 0;
+	}
+</style>
