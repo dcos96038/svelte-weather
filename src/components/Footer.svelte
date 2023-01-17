a685e8f261b0f5ae154b8cef01aa15ecf41c2d94
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2023-01-17 17:59:43 -0300

    Render weather info and added a footer component with props

---
+++ src/components/Footer.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let humidity: number
+  export let windSpeed: number
+  export let feelsLike: number
+</script>
+
+<div class="flex justify-center w-full absolute right-0 left-0 bottom-14">
+  <div
+    class="flex bg-white text-black drop-shadow-sm gap-4 justify-around md:w-10/12 border rounded-md py-2 px-4"
+  >
+    <div class="flex flex-col items-center">
+      <h6 class="text-lg font-bold">{humidity}%</h6>
+      <span class="text-sm font-semibold">Humidity</span>
+    </div>
+    <div class="flex flex-col items-center">
+      <h6 class="text-lg font-bold">{windSpeed} km/h</h6>
+      <span class="text-sm font-semibold">Wind Speed</span>
+    </div>
+    <div class="flex flex-col items-center">
+      <h6 class="text-lg font-bold">{feelsLike}°</h6>
+      <span class="text-sm font-semibold">Feels Like</span>
+    </div>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,27 @@
-<script lang='ts'>
-	import type { PageServerData } from "./$types";
+<script lang="ts">
+	import Footer from '../components/Footer.svelte';
+	import type { IWeather } from 'src/types/weather';
+	import type { PageServerData } from './$types';
 
-	export let data: PageServerData
+	export let data: PageServerData;
 
-	const weatherData = data
+	const weatherData: IWeather = data.weather;
 </script>
 
-<h1>Hola Mundo</h1>
+<div
+	class="relative h-5/6 md:w-3/6 md:bg-slate-600 md:border rounded-md w-full md:border-black md:drop-shadow-md py-8 px-10"
+>
+	<section class="flex md:justify-between flex-col md:flex-row">
+		<div class="flex flex-col">
+			<h2 class="uppercase md:text-xl text-lg mb-2">{weatherData.locationName}</h2>
+			<h1 class="md:text-8xl text-6xl font-bold">{weatherData.temperature}°</h1>
+		</div>
+		<div class="flex justify-start items-center md:flex-row-reverse">
+			<h3 class="font-semibold md:transform md:-rotate-90 text-2xl md:cursor-vertical-text">
+				{weatherData.conditionText}
+			</h3>
+			<img src={weatherData.conditionIcon} class="md:w-28" alt={weatherData.conditionText} />
+		</div>
+	</section>
+	<Footer humidity={weatherData.humidity} feelsLike={weatherData.feelsLike} windSpeed={weatherData.windSpeed} />
+</div>
