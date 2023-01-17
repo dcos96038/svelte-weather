7ac84c980e28648b1f030236621a9d31688f702d
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2023-01-17 17:58:52 -0300

    Added Weather Data Type

--- src/types/weather.ts
+++ src/types/weather.ts
@@ -1,48 +1,61 @@
 export interface WeatherResponse {
-  location: Location;
-  current: Current;
+	location: Location;
+	current: Current;
 }
 
 interface Current {
-  last_updated_epoch: number;
-  last_updated: string;
-  temp_c: number;
-  temp_f: number;
-  is_day: number;
-  condition: Condition;
-  wind_mph: number;
-  wind_kph: number;
-  wind_degree: number;
-  wind_dir: string;
-  pressure_mb: number;
-  pressure_in: number;
-  precip_mm: number;
-  precip_in: number;
-  humidity: number;
-  cloud: number;
-  feelslike_c: number;
-  feelslike_f: number;
-  vis_km: number;
-  vis_miles: number;
-  uv: number;
-  gust_mph: number;
-  gust_kph: number;
+	last_updated_epoch: number;
+	last_updated: string;
+	temp_c: number;
+	temp_f: number;
+	is_day: number;
+	condition: Condition;
+	wind_mph: number;
+	wind_kph: number;
+	wind_degree: number;
+	wind_dir: string;
+	pressure_mb: number;
+	pressure_in: number;
+	precip_mm: number;
+	precip_in: number;
+	humidity: number;
+	cloud: number;
+	feelslike_c: number;
+	feelslike_f: number;
+	vis_km: number;
+	vis_miles: number;
+	uv: number;
+	gust_mph: number;
+	gust_kph: number;
 }
 
 interface Condition {
-  text: string;
-  icon: string;
-  code: number;
+	text: string;
+	icon: string;
+	code: number;
 }
 
 interface Location {
-  name: string;
-  region: string;
-  country: string;
-  lat: number;
-  lon: number;
-  tz_id: string;
-  localtime_epoch: number;
-  localtime: string;
+	name: string;
+	region: string;
+	country: string;
+	lat: number;
+	lon: number;
+	tz_id: string;
+	localtime_epoch: number;
+	localtime: string;
 }
 
+export interface IWeather {
+	conditionIcon: string;
+	conditionText: string;
+	country: string;
+	localtime: string;
+	locationName: string;
+	humidity: number;
+	isDay: number;
+	feelsLike: number;
+	temperature: number;
+	windSpeed: number;
+	windDir: string;
+}
